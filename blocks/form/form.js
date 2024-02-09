a8eb0cc4880ec89b46ce8eb1c3cf962229f972ec
rogan-s <158459146+rogan-s@users.noreply.github.com>
2024-02-09 07:47:10 +0530

    Update form.js

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -1,229 +1,108 @@
-import { toClassName } from '../../scripts/aem.js';
+import createField from './form-fields.js';
+import { sampleRUM } from '../../scripts/aem.js';
 
-function createFieldWrapper(fd) {
-  const fieldWrapper = document.createElement('div');
-  if (fd.Style) fieldWrapper.className = fd.Style;
-  fieldWrapper.classList.add('field-wrapper', `${fd.Type}-wrapper`);
+async function createForm(formHref) {
+  const { pathname } = new URL(formHref);
+  const resp = await fetch(pathname);
+  const json = await resp.json();
 
-  fieldWrapper.dataset.fieldset = fd.Fieldset;
+  const form = document.createElement('form');
+  // eslint-disable-next-line prefer-destructuring
+  form.dataset.action = pathname.split('.json')[0];
 
-  return fieldWrapper;
-}
-
-const ids = [];
-function generateFieldId(fd, suffix = '') {
-  const slug = toClassName(`form-${fd.Name}${suffix}`);
-  ids[slug] = ids[slug] || 0;
-  const idSuffix = ids[slug] ? `-${ids[slug]}` : '';
-  ids[slug] += 1;
-  return `${slug}${idSuffix}`;
-}
-
-function createLabel(fd) {
-  const label = document.createElement('label');
-  label.id = generateFieldId(fd, '-label');
-  label.textContent = fd.Label || fd.Name;
-  label.setAttribute('for', fd.Id);
-  return label;
-}
-
-function setCommonAttributes(field, fd) {
-  field.id = fd.Id;
-  field.name = fd.Name;
-  field.required = fd.Mandatory && (fd.Mandatory.toLowerCase() === 'true' || fd.Mandatory.toLowerCase() === 'x');
-  field.placeholder = fd.Placeholder;
-  field.value = fd.Value;
-}
-
-const createHeading = (fd) => {
-  const fieldWrapper = createFieldWrapper(fd);
-
-  const level = fd.Style && fd.Style.includes('sub-heading') ? 3 : 2;
-  const heading = document.createElement(`h${level}`);
-  heading.textContent = fd.Value || fd.Label;
-  heading.id = fd.Id;
-
-  fieldWrapper.append(heading);
-
-  return { field: heading, fieldWrapper };
-};
-
-const createPlaintext = (fd) => {
-  const fieldWrapper = createFieldWrapper(fd);
-
-  const text = document.createElement('p');
-  text.textContent = fd.Value || fd.Label;
-  text.id = fd.Id;
-
-  fieldWrapper.append(text);
-
-  return { field: text, fieldWrapper };
-};
-
-const createSelect = async (fd) => {
-  const select = document.createElement('select');
-  setCommonAttributes(select, fd);
-  const addOption = ({ text, value }) => {
-    const option = document.createElement('option');
-    option.text = text.trim();
-    option.value = value.trim();
-    if (option.value === select.value) {
-      option.setAttribute('selected', '');
+  const fields = await Promise.all(json.data.map((fd) => createField(fd, form)));
+  fields.forEach((field) => {
+    if (field) {
+      form.append(field);
     }
-    select.add(option);
-    return option;
-  };
-
-  if (fd.Placeholder) {
-    const ph = addOption({ text: fd.Placeholder, value: '' });
-    ph.setAttribute('disabled', '');
-  }
-
-  if (fd.Options) {
-    let options = [];
-    if (fd.Options.startsWith('https://')) {
-      const optionsUrl = new URL(fd.Options);
-      const resp = await fetch(`${optionsUrl.pathname}${optionsUrl.search}`);
-      const json = await resp.json();
-      json.data.forEach((opt) => {
-        options.push({
-          text: opt.Option,
-          value: opt.Value || opt.Option,
-        });
-      });
-    } else {
-      options = fd.Options.split(',').map((opt) => ({
-        text: opt.trim(),
-        value: opt.trim().toLowerCase(),
-      }));
-    }
-
-    options.forEach((opt) => addOption(opt));
-  }
-
-  const fieldWrapper = createFieldWrapper(fd);
-  fieldWrapper.append(select);
-  fieldWrapper.append(createLabel(fd));
-
-  return { field: select, fieldWrapper };
-};
-
-const createConfirmation = (fd, form) => {
-  form.dataset.confirmation = new URL(fd.Value).pathname;
-
-  return {};
-};
-
-const createSubmit = (fd) => {
-  const button = document.createElement('button');
-  button.textContent = fd.Label || fd.Name;
-  button.classList.add('button');
-  button.type = 'submit';
-
-  const fieldWrapper = createFieldWrapper(fd);
-  fieldWrapper.append(button);
-  return { field: button, fieldWrapper };
-};
-
-const createTextArea = (fd) => {
-  const field = document.createElement('textarea');
-  setCommonAttributes(field, fd);
-
-  const fieldWrapper = createFieldWrapper(fd);
-  const label = createLabel(fd);
-  field.setAttribute('aria-labelledby', label.id);
-  fieldWrapper.append(field);
-  fieldWrapper.append(label);
-
-  return { field, fieldWrapper };
-};
-
-const createInput = (fd) => {
-  const field = document.createElement('input');
-  field.type = fd.Type;
-  setCommonAttributes(field, fd);
-
-  const fieldWrapper = createFieldWrapper(fd);
-  const label = createLabel(fd);
-  field.setAttribute('aria-labelledby', label.id);
-  fieldWrapper.append(field);
-  fieldWrapper.append(label);
-
-  return { field, fieldWrapper };
-};
-
-const createFieldset = (fd) => {
-  const field = document.createElement('fieldset');
-  setCommonAttributes(field, fd);
-
-  if (fd.Label) {
-    const legend = document.createElement('legend');
-    legend.textContent = fd.Label;
-    field.append(legend);
-  }
-
-  const fieldWrapper = createFieldWrapper(fd);
-  fieldWrapper.append(field);
-
-  return { field, fieldWrapper };
-};
-
-const createToggle = (fd) => {
-  const { field, fieldWrapper } = createInput(fd);
-  field.type = 'checkbox';
-  if (!field.value) field.value = 'on';
-  field.classList.add('toggle');
-  fieldWrapper.classList.add('selection-wrapper');
-
-  const toggleSwitch = document.createElement('div');
-  toggleSwitch.classList.add('switch');
-  toggleSwitch.append(field);
-  fieldWrapper.append(toggleSwitch);
-
-  const slider = document.createElement('span');
-  slider.classList.add('slider');
-  toggleSwitch.append(slider);
-  slider.addEventListener('click', () => {
-    field.checked = !field.checked;
   });
 
-  return { field, fieldWrapper };
-};
+  // group fields into fieldsets
+  const fieldsets = form.querySelectorAll('fieldset');
+  fieldsets.forEach((fieldset) => {
+    form.querySelectorAll(`[data-fieldset="${fieldset.name}"`).forEach((field) => {
+      fieldset.append(field);
+    });
+  });
 
-const createCheckbox = (fd) => {
-  const { field, fieldWrapper } = createInput(fd);
-  if (!field.value) field.value = 'checked';
-  fieldWrapper.classList.add('selection-wrapper');
+  return form;
+}
 
-  return { field, fieldWrapper };
-};
+function generatePayload(form) {
+  const payload = {};
 
-const createRadio = (fd) => {
-  const { field, fieldWrapper } = createInput(fd);
-  if (!field.value) field.value = fd.Label || 'on';
-  fieldWrapper.classList.add('selection-wrapper');
+  [...form.elements].forEach((field) => {
+    if (field.name && field.type !== 'submit' && !field.disabled) {
+      if (field.type === 'radio') {
+        if (field.checked) payload[field.name] = field.value;
+      } else if (field.type === 'checkbox') {
+        if (field.checked) payload[field.name] = payload[field.name] ? `${payload[field.name]},${field.value}` : field.value;
+      } else {
+        payload[field.name] = field.value;
+      }
+    }
+  });
+  return payload;
+}
 
-  return { field, fieldWrapper };
-};
+function handleSubmitError(form, error) {
+  // eslint-disable-next-line no-console
+  console.error(error);
+  form.querySelector('button[type="submit"]').disabled = false;
+  sampleRUM('form:error', { source: '.form', target: error.stack || error.message || 'unknown error' });
+}
 
-const FIELD_CREATOR_FUNCTIONS = {
-  select: createSelect,
-  heading: createHeading,
-  plaintext: createPlaintext,
-  'text-area': createTextArea,
-  toggle: createToggle,
-  submit: createSubmit,
-  confirmation: createConfirmation,
-  fieldset: createFieldset,
-  checkbox: createCheckbox,
-  radio: createRadio,
-};
+async function handleSubmit(form) {
+  if (form.getAttribute('data-submitting') === 'true') return;
 
-export default async function createField(fd, form) {
-  fd.Id = fd.Id || generateFieldId(fd);
-  const type = fd.Type.toLowerCase();
-  const createFieldFunc = FIELD_CREATOR_FUNCTIONS[type] || createInput;
-  const fieldElements = await createFieldFunc(fd, form);
+  const submit = form.querySelector('button[type="submit"]');
+  try {
+    form.setAttribute('data-submitting', 'true');
+    submit.disabled = true;
 
-  return fieldElements.fieldWrapper;
+    // create payload
+    const payload = generatePayload(form);
+    const response = await fetch(form.dataset.action, {
+      method: 'POST',
+      body: JSON.stringify({ data: payload }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      sampleRUM('form:submit', { source: '.form', target: form.dataset.action });
+      if (form.dataset.confirmation) {
+        window.location.href = form.dataset.confirmation;
+      }
+    } else {
+      const error = await response.text();
+      throw new Error(error);
+    }
+  } catch (e) {
+    handleSubmitError(form, e);
+  } finally {
+    form.setAttribute('data-submitting', 'false');
+  }
+}
+
+export default async function decorate(block) {
+  const formLink = block.querySelector('a[href$=".json"]');
+  if (!formLink) return;
+
+  const form = await createForm(formLink.href);
+  block.replaceChildren(form);
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const valid = form.checkValidity();
+    if (valid) {
+      handleSubmit(form);
+    } else {
+      const firstInvalidEl = form.querySelector(':invalid:not(fieldset)');
+      if (firstInvalidEl) {
+        firstInvalidEl.focus();
+        firstInvalidEl.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  });
 }
